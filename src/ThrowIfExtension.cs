650f6e7362ad76d868d883276ed91e7be6b6483a
dtopiast <dtopiast@protonmail.com>
2023-09-02 16:37:34 -0600

    Agregar archivos de proyecto.

---
+++ src/ThrowIfExtension.cs
@@ -0,0 +1,47 @@
+﻿namespace OnionCrafter.Utils.Exception
+{
+    /// <summary>
+    /// Provides extension methods for throwing exceptions based on conditions.
+    /// </summary>
+    public static class ThrowIfExtension
+    {
+        /// <summary>
+        /// Throws an exception of type <typeparamref name="TException"/> if the specified condition is met.
+        /// </summary>
+        /// <typeparam name="T">The type of the property.</typeparam>
+        /// <typeparam name="TException">The type of the exception to throw.</typeparam>
+        /// <param name="prop">The property to check.</param>
+        /// <param name="condition">The condition to evaluate.</param>
+        /// <returns>The value of the property.</returns>
+        /// <exception cref="System.Exception">Thrown when the <paramref name="condition"/> is met.</exception>
+        public static T? ThrowIf<T, TException>(this T? prop, bool condition)
+            where TException : System.Exception
+        {
+            if (condition)
+            {
+                throw Activator.CreateInstance(typeof(TException), nameof(prop)) as TException ?? throw new ApplicationException();
+            }
+            return prop;
+        }
+
+        /// <summary>
+        /// Throws an exception of type <typeparamref name="TException"/> if the specified condition is met.
+        /// </summary>
+        /// <typeparam name="T">The type of the property.</typeparam>
+        /// <typeparam name="TException">The type of the exception to throw.</typeparam>
+        /// <param name="prop">The property to check.</param>
+        /// <param name="condition">The condition to evaluate.</param>
+        /// <param name="args">Additional arguments to pass to the exception constructor.</param>
+        /// <returns>The value of the property.</returns>
+        /// <exception cref="System.Exception">Thrown when the <paramref name="condition"/> is met.</exception>
+        public static T? ThrowIf<T, TException>(this T? prop, bool condition, params object?[] args)
+            where TException : System.Exception
+        {
+            if (condition)
+            {
+                throw Activator.CreateInstance(typeof(TException), args) as TException ?? throw new ApplicationException();
+            }
+            return prop;
+        }
+    }
+}
---
+++ src/ThrowIfNullExtension.cs
@@ -0,0 +1,56 @@
+﻿namespace OnionCrafter.Utils.Exception
+{
+    /// <summary>
+    /// Provides extension methods for handling exceptions.
+    /// </summary>
+#pragma warning disable CS8603 // Posible tipo de valor devuelto de referencia nulo
+
+    public static class ThrowIfNullExtension
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException"/> if the specified object is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <param name="prop">The object to check for null.</param>
+        /// <returns>The object if it is not null.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the object is null.</exception>
+        public static T ThrowIfNull<T>(this T? prop)
+        {
+            prop.ThrowIf<T, ArgumentNullException>(prop == null);
+            return prop;
+        }
+
+        /// <summary>
+        /// Throws a specified exception if the specified object is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <typeparam name="TException">The type of exception to be thrown.</typeparam>
+        /// <param name="prop">The object to check for null.</param>
+        /// <returns>The object if it is not null.</returns>
+        /// <exception>Thrown when the object is null.</exception>
+        public static T ThrowIfNull<T, TException>(this T? prop)
+            where TException : System.Exception
+        {
+            prop.ThrowIf<T, TException>(prop == null);
+            return prop;
+        }
+
+        /// <summary>
+        /// Throws a specified exception with params if the specified object is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the object.</typeparam>
+        /// <typeparam name="TException">The type of exception to be thrown.</typeparam>
+        /// <param name="prop">The object to check for null.</param>
+        /// <param name="args">The params for create the exception</param>
+        /// <returns>The object if it is not null.</returns>
+        /// <exception>Thrown when the object is null.</exception>
+        public static T ThrowIfNull<T, TException>(this T? prop, params object?[] args)
+                where TException : System.Exception
+        {
+            prop.ThrowIf<T, TException>(prop == null, args);
+            return prop;
+        }
+    }
+
+#pragma warning restore CS8603 // Posible tipo de valor devuelto de referencia nulo
+}
